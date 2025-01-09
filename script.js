461f71bed08742ce84a3aba500e03ce757f70d7e
Caryensong <caryensong@googlemail.com>
2025-01-09 10:06:17 +0100

    update pokeimg background style

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ function  getPokedexTemplate(pokemon, i, img, type){
    return `
    <div class="pokemon_box">
            <div class="box_headline ${type.name}"></div>
-            <div class="pokeimg_bg ${type.name}">
-            <img class="pokeimg_b_img" src="./assets/icon/${type.name}_icon.png" alt="${type.name}"></div>
+            <div class="pokeimg_bg ${type.name}"></div>
+            <img class="pokeimg_b_img" src="./assets/icon/${type.name}_icon.png" alt="${type.name}">
             <img class="pokemon_img" src="${img.front_default}" alt="Pokemon">
              <span class="poke_Name"># ${i +1} ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</span>
       <div id="elementContent${i}" class="element_content"></div>
